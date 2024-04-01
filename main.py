08a8bdcb15e63cb98a01ef30cb1b8d4b7cce0e9e
Mooton PK <mootonpk@Mootons-MacBook-Air.local>
2024-04-01 13:20:52 +0700

    first

---
+++ main.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+import requests
+
+URL = "https://ทองคําราคา.com"
+
+result = requests.get(URL)
+doc = BeautifulSoup(result.text, "html.parser")
+
+price_tag = doc.find_all(["td"] ,string="ทองคำแท่ง")
+parent = price_tag[0].parent
+price = parent.find_all(["td"], class_="em bg-em g-d")[1]
+
+print(price)
